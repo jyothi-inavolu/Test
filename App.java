8cafbfb541f52ba6819ed5432e55fc12e0ac1afa
Jyothi Sree <jyothi.inavolu@games24x7.com>
2016-05-16 18:05:58 +0530

    Initial commit

---
+++ App.java
@@ -0,0 +1,43 @@
+package com.games24x7.Test;
+
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String args[])
+    {
+	List l1=getLinkedList();
+	if(l1 instanceof ArrayList)
+	{
+	System.out.println("This is arrayList");	
+	
+    }
+	if(l1 instanceof LinkedList)
+    {
+    	System.out.println("This is LinkedList");
+    }
+    }
+
+public static List getArrayList()
+{
+	List l=new ArrayList();
+	return l;
+}
+
+public static List getLinkedList()
+{
+	List l=new LinkedList();
+	return l;
+}
+}
